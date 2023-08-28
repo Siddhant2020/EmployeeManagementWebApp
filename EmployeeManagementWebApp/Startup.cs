fe60f69347d89e1eedb03232a623036250b90bae
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-08-28 23:01:30 +0530

    Revision session @MVC

--- EmployeeManagementWebApp/Startup.cs
+++ EmployeeManagementWebApp/Startup.cs
@@ -15,13 +15,14 @@ namespace EmployeeManagementWebApp
 {
     public class Startup
     {
+        public IConfiguration _configuration { get; }
+
         //Constructor injection to inject IConfiguration service
         public Startup(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
-        public IConfiguration _configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -49,18 +50,20 @@ namespace EmployeeManagementWebApp
             //    app.UseExceptionHandler("/Error");
             //}
 
+
             //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
             //defaultFilesOptions.DefaultFileNames.Clear();
-            //defaultFilesOptions.DefaultFileNames.Add("foo.html");
-            //app.UseDefaultFiles(defaultFilesOptions);
-            //app.UseStaticFiles();
+            //defaultFilesOptions.DefaultFileNames.Add("foo.html"); //set foo.html as default page
+            //app.UseDefaultFiles(defaultFilesOptions); //UseDefaultFiles Middleware is used to set any default web page apart from the conventional approach(i.e. default.html, default.htm) by using the DefaultFilesOptions object
+            //app.UseStaticFiles(); //To serve static files UseStaticFiles middleware is required, files which is placed inside wwwroot folder
 
+            //FileServer Middleware is the replacement of both UseDefaultFiles, UseStaticFiles Middlewares
             //FileServerOptions fileServerOptions = new FileServerOptions();
             //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
             //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
             //app.UseFileServer(fileServerOptions);
 
-            app.UseMvc();
+            //app.UseMvc();
 
             //app.Run(async (context) =>
             //{
@@ -77,17 +80,18 @@ namespace EmployeeManagementWebApp
             //    logger.LogInformation("MW1 : Outgoing Response");
             //});
 
-            //app.Use(async (context, next) => {
+            //app.Use(async (context, next) =>
+            //{
             //    logger.LogInformation("MW2 : Incoming Request");
-            //    //await context.Response.WriteAsync("Hello World from 1st Middleware \n");
+            //    await context.Response.WriteAsync("Hello World from 1st Middleware \n");
             //    await next();
             //    logger.LogInformation("MW2 : Outgoing Response");
             //});
 
-            //app.Run(async (context) => {
-
+            //app.Run(async (context) =>
+            //{
             //    await context.Response.WriteAsync("Hello World from 3rd Middleware");
-            //    logger.LogInformation("MW3: Request handled and response produced");
+            //    //logger.LogInformation("MW3: Request handled and response produced");
             //    //await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);    //to get the server process name the application is running upon
             //    //await context.Response.WriteAsync(_configuration["MyKey"]);   //to get the value from configuration file
             //});
