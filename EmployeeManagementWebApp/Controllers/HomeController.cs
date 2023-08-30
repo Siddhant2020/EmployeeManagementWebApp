42ccdf01403fb2ca80a17eb98ed8b7577ddeb929
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-08-30 09:27:58 +0530

    Controller returns View

--- EmployeeManagementWebApp/Controllers/HomeController.cs
+++ EmployeeManagementWebApp/Controllers/HomeController.cs
@@ -17,12 +17,12 @@ namespace EmployeeManagementWebApp.Controllers
             return _employeeRepository.GetEmployee(1).Name;
         }
 
-        public JsonResult Details(int id)
+        public ViewResult Details(int id)
         {
             Employee model = _employeeRepository.GetEmployee(id);
-            return Json(model);
-        }
+            return View(model);
 
+        }
         //public JsonResult Index()
         //{
         //    //return View();
