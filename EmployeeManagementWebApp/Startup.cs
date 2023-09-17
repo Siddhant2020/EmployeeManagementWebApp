94340c0581cfab2db2f68700456c22872c3452af
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-09-17 09:12:50 +0530

    AddSingleton vs AddScoped vs AddTransient

--- EmployeeManagementWebApp/Startup.cs
+++ EmployeeManagementWebApp/Startup.cs
@@ -29,7 +29,7 @@ namespace EmployeeManagementWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             IMvcBuilder mvcBuilder = services.AddMvc(options => options.EnableEndpointRouting = false).AddXmlSerializerFormatters();
-            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddTransient<IEmployeeRepository, MockEmployeeRepository>();
             //services.AddRazorPages();
             //MvcOptions options = new MvcOptions();
             //options.EnableEndpointRouting = false;
