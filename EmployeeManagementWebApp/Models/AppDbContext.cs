185ad14abad1e947c7c89852d66df479eec36fba
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-09-20 23:02:55 +0530

    DbContext in entity framework core

---
+++ EmployeeManagementWebApp/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementWebApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+                
+        }
+
+        DbSet<Employee> Employees { get; set;}
+    }
+}
