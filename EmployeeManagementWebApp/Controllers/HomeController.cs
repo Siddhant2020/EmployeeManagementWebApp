557cc3a8c5aa46af3aefcfe5656f21bbf23bea3a
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-09-26 07:56:15 +0530

    File Upload in ASP.NET Core MVC

--- EmployeeManagementWebApp/Controllers/HomeController.cs
+++ EmployeeManagementWebApp/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using EmployeeManagementWebApp.Models;
 using EmployeeManagementWebApp.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 
 namespace EmployeeManagementWebApp.Controllers
 {
@@ -8,10 +11,13 @@ namespace EmployeeManagementWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IWebHostEnvironment _webHostingEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository,
+            IWebHostEnvironment webHostingEnvironment)
         {
             _employeeRepository = employeeRepository;
+            _webHostingEnvironment = webHostingEnvironment;
         }
 
         //[Route("~/")]   //Map via Attribute Routing when url just contains the domain name http://localhost:38315/ or http://localhost:38315
@@ -47,12 +53,36 @@ namespace EmployeeManagementWebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Employee employee)
+        public IActionResult Create(EmployeeCreateViewModel employeeModel)
         {
             if (ModelState.IsValid)
             {
+                string uniqueFileName = null;
+                // If the Photo property on the incoming model object is not null, then the user has selected an image to upload.
+                if (employeeModel.Photo != null)
+                {
+                    // The image must be uploaded to the images folder in wwwroot. To get the path of the wwwroot folder we are using the inject
+                    // WebHostingEnvironment service provided by ASP.NET Core
+
+                    string uploadsFolder = Path.Combine(_webHostingEnvironment.WebRootPath, "images");
+
+                    // To make sure the file name is unique we are appending a new GUID value and and an underscore to the file name
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + employeeModel.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    // Use CopyTo() method provided by IFormFile interface to copy the file to wwwroot/images folder
+                    employeeModel.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+                Employee employee = new Employee
+                {
+                    Name = employeeModel.Name,
+                    Email = employeeModel.Email,
+                    Department = employeeModel.Department,
+                    // Use CopyTo() method provided by IFormFile interface to copy the file to wwwroot/images folder
+                    PhotoPath = uniqueFileName
+                };
                 Employee newEmployee = _employeeRepository.Add(employee);
-                //return RedirectToAction("details", new { id = newEmployee.Id });
+                return RedirectToAction("details", new { id = newEmployee.Id });
             }
             return View();
         }
