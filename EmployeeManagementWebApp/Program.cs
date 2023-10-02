ce3260c5deaf1473c2234bfc3cb3885608b54110
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-10-02 09:51:18 +0530

    Logging in File in ASP.NET Core using nlog

--- EmployeeManagementWebApp/Program.cs
+++ EmployeeManagementWebApp/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,15 +21,23 @@ namespace EmployeeManagementWebApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                    logging.AddNLog();
+                })
                 .UseStartup<Startup>();
 
 
         //public static IHostBuilder CreateWebHostBuilder(string[] args) =>
         //    Host.CreateDefaultBuilder(args)
-                //.ConfigureWebHostDefaults(webBuilder =>
-                //{
-                //    webBuilder.UseStartup<Startup>();
-                //});
+        //.ConfigureWebHostDefaults(webBuilder =>
+        //{
+        //    webBuilder.UseStartup<Startup>();
+        //});
 
 
         //below is the code to use any configuration file instead appsettings.json by mentioning in configureAppconfiguration method
