0e04b2633702b8844422ed3ea4b5f546443ae042
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-10-04 08:50:23 +0530

    User Registration in ASP.NET MVC

---
+++ EmployeeManagementWebApp/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagementWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Register(RegisterViewModel registerViewModel)
+        //{
+        //    return View();
+        //}
+    }
+}
