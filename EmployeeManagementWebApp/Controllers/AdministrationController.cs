3aa1e456936912a3b42a8ebfb8feb5f8da26a8b1
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-10-22 08:50:09 +0530

    Creating Roles in ASP.Net Core

---
+++ EmployeeManagementWebApp/Controllers/AdministrationController.cs
@@ -0,0 +1,55 @@
+﻿using EmployeeManagementWebApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementWebApp.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel createRoleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // We just need to specify a unique role name to create a new role
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = createRoleViewModel.RoleName
+                };
+
+                // Saves the role in the underlying AspNetRoles table
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(createRoleViewModel);
+        }
+    }
+}
