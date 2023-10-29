345ea8352df0f4a15e0abb92b9ecd1c690d236b4
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2023-10-29 16:08:45 +0530

    delete user confirmation in ASP.Net Core

--- EmployeeManagementWebApp/wwwroot/js/CustomScript.js
+++ EmployeeManagementWebApp/wwwroot/js/CustomScript.js
@@ -1,1 +1,12 @@
-﻿
+﻿function confirmDelete(uniqueId, isdeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (isdeleteClicked) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
